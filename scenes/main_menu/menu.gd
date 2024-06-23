3723e6fca6a9b8a8e2edaeef376ddbb27bb88543
Lukas <lukas.baltes@gmx.de>
2024-06-23 15:17:58 +0200

    Add load button in main menu

--- scenes/main_menu/menu.gd
+++ scenes/main_menu/menu.gd
@@ -1,12 +1,13 @@
 extends Control
 
 
-# temporary. TODO: remove
 func _ready() -> void:
-	call_deferred("_on_play_pressed", 1)
+	#call_deferred("_on_play_pressed", 1) # temporary. TODO: remove
+	
+	$CenterContainer/VBoxContainer/VBoxContainer/ContinueButton.visible = Savestate.exists()
 
 
-func _on_play_pressed(colors: int):
+func _on_play_pressed(colors: int) -> void:
 	match colors:
 		1:
 			Gamestate.reset(Gamestate.Mode.SINGLE_COLOR)
@@ -17,11 +18,22 @@ func _on_play_pressed(colors: int):
 		_:
 			Gamestate.reset() # safety. shouldn't happen though
 	
+	_open_gamescreen()
+
+
+func _on_continue_pressed() -> void:
+	if not Savestate.exists(): # safety
+		return
+	
+	Gamestate.load()
+	_open_gamescreen()
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+
+func _open_gamescreen():
 	get_tree().change_scene_to_file("res://scenes/game/gamescreen.tscn")
 	#var asdf = preload("res://gamescreen.tscn") #.instantiate()
 	#get_tree().change_scene_to_packed(asdf)
-
-
-func _on_quit_pressed():
-	get_tree().quit()
-
